7ab97c56714b7ce53cbe2d6f186e141cb4637145
John McDonald <96137405+jmcied@users.noreply.github.com>
2023-01-17 19:14:02 +0000

    basic structure

---
+++ src/models/mem/playlist-mem-store.js
@@ -0,0 +1,29 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { v4 } from "uuid";
+
+let playlists = [];
+
+export const playlistMemStore = {
+  async getAllPlaylists() {
+    return playlists;
+  },
+
+  async addPlaylist(playlist) {
+    playlist._id = v4();
+    playlists.push(playlist);
+    return playlist;
+  },
+
+  async getPlaylistById(id) {
+    return playlists.find((playlist) => playlist._id === id);
+  },
+
+  async deletePlaylistById(id) {
+    const index = playlists.findIndex((playlist) => playlist._id === id);
+    playlists.splice(index, 1);
+  },
+
+  async deleteAllPlaylists() {
+    playlists = [];
+  },
+};
