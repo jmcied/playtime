7d57ce02ae406add733740325a1aef56c0f24a19
John McDonald <96137405+jmcied@users.noreply.github.com>
2023-01-19 19:30:00 +0000

    user sessions, authentication, global playlist

--- src/controllers/accounts-controller.js
+++ src/controllers/accounts-controller.js
@@ -2,16 +2,22 @@ import { db } from "../models/db.js";
 
 export const accountsController = {
   index: {
+    // disable the strategy for the accounts routes, 
+    // This turns off the session strategy - 
+    // so these routes can work (and the users can signup/login)
+    auth: false,                          
     handler: function (request, h) {
       return h.view("main", { title: "Welcome to Playlist" });
     },
   },
   showSignup: {
+    auth: false,
     handler: function (request, h) {
       return h.view("signup-view", { title: "Sign up for Playlist" });
     },
   },
   signup: {
+    auth: false,
     handler: async function (request, h) {
       const user = request.payload;
       await db.userStore.addUser(user);
@@ -19,23 +25,34 @@ export const accountsController = {
     },
   },
   showLogin: {
+    auth: false,
     handler: function (request, h) {
       return h.view("login-view", { title: "Login to Playlist" });
     },
   },
   login: {
+    auth: false,
     handler: async function (request, h) {
       const { email, password } = request.payload;
       const user = await db.userStore.getUserByEmail(email);
       if (!user || user.password !== password) {
         return h.redirect("/");
       }
+      request.cookieAuth.set({ id: user._id });
       return h.redirect("/dashboard");
     },
   },
   logout: {
+    auth: false,
     handler: function (request, h) {
       return h.redirect("/");
     },
+  },
+  async validate(request, session) {
+    const user = await db.userStore.getUserById(session.id);
+    if (!user) {
+      return { isValid: false };
+    }
+    return { isValid: true, credentials: user };
   },
 };
--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import Hapi from "@hapi/hapi";
 import Vision from "@hapi/vision";
+import Cookie from "@hapi/cookie";
 import Handlebars from "handlebars";
 import path from "path";
 import { fileURLToPath } from "url";
 import { webRoutes } from "./web-routes.js";
 import { db } from "./models/db.js";
+import { accountsController } from "./controllers/accounts-controller.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -27,6 +29,17 @@ async function init() {
     layout: true,
     isCached: false,
   });
+  await server.register(Cookie);
+  server.auth.strategy("session", "cookie", {
+    cookie: {
+      name: "playtime",
+      password: "secretpasswordnotrevealedtoanyone",
+      isSecure: false,
+    },
+    redirectTo: "/",
+    validate: accountsController.validate,
+  });
+  server.auth.default("session");
   db.init();
   server.route(webRoutes);
   await server.start();
