ef6b9f320cbf15d1c16840de3c108a13b980d14b
John McDonald <96137405+jmcied@users.noreply.github.com>
2023-01-24 14:02:53 +0000

    User login validation

--- src/models/joi-schemas.js
+++ src/models/joi-schemas.js
@@ -7,3 +7,18 @@ export const UserSpec = {
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 };
+
+export const UserCredentialsSpec = {
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+};
+
+export const TrackSpec = {
+  title: Joi.string().required(),
+  artist: Joi.string().required(),
+  duration: Joi.number(),
+};
+
+export const PlaylistSpec = {
+  title: Joi.string().required(),
+};
