f500b793e7a7a9d540a325c3a7e2ba6d3b2b5a12
John McDonald <96137405+jmcied@users.noreply.github.com>
2023-01-27 16:25:12 +0000

    Unit test using MemStore Users

--- src/models/db.js
+++ src/models/db.js
@@ -1,19 +1,26 @@
-// import { userMemStore } from "./mem/user-mem-store.js";
-// import { playlistMemStore } from "./mem/playlist-mem-store.js";
-// import { trackMemStore } from "./mem/track-mem-store.js";
+ import { userMemStore } from "./mem/user-mem-store.js";
+ import { playlistMemStore } from "./mem/playlist-mem-store.js";
+import { trackMemStore } from "./mem/track-mem-store.js";
 
-import { userJsonStore } from "./json/user-json-store.js";
-import { playlistJsonStore } from "./json/playlist-json-store.js";
-import { trackJsonStore } from "./json/track-json-store.js";
+// import { userJsonStore } from "./json/user-json-store.js";
+// import { playlistJsonStore } from "./json/playlist-json-store.js";
+// import { trackJsonStore } from "./json/track-json-store.js";
 
 export const db = {
   userStore: null,
   playlistStore: null,
   trackStore: null, 
-
+/*
   init() {
     this.userStore = userJsonStore;                  // change from userMemStore
     this.playlistStore = playlistJsonStore;
     this.trackStore = trackJsonStore;
+  }, */
+
+  init() {
+    this.userStore = userMemStore;                  // week 2 exercise 1
+    this.playlistStore = playlistMemStore;
+    this.trackStore = trackMemStore;
   },
+
 };
